a1d4d36d091a2ba0e13ad394af4a18956566db84
Grzegorz Brzozowski <Dolidius@gmail.com>
2021-02-10 20:05:52 +0100

    lepsze triggery

--- triggers.sql
+++ triggers.sql
@@ -9,11 +9,33 @@ IF UPDATE(Treść) BEGIN
 END
 GO
 
+--------------------------------------------------------------------------------
+
 CREATE TRIGGER ArchiwizujPostyDelete
 ON Posty
-AFTER DELETE 
+INSTEAD OF DELETE 
 AS
+
+DECLARE @DeletedID INT
+SET @DeletedID = (
+    SELECT Id FROM Deleted
+)
+
+IF ((SELECT COUNT (*) FROM Posty P WHERE P.Id = @DeletedID) = 0) BEGIN
+    ROLLBACK
+    RAISERROR('Nie istnieje post który próbowano usunąć', 16, 1)
+END
+
+IF ((SELECT COUNT (*) FROM Posty P WHERE P.Id = @DeletedID) > 1) BEGIN
+    ROLLBACK
+    RAISERROR('Można usunąć tylko jeden post naraz', 16, 1)
+END
+
+DELETE FROM Posty
+WHERE Id = @DeletedID
+
 INSERT INTO Posty_Archiwum VALUES (Treść, Id_Autora, Id_Grupy, Ilość_Polubień, Data_Dodania, GETDATE(), 'usunięcie') 
 SELECT Treść, Id_Autora, Id_Grupy, Ilość_Polubień, Data_Dodania
 FROM Deleted 
+
 GO
