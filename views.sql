675546664c3612581a5b28bab597e79204d52133
Grzegorz Brzozowski <Dolidius@gmail.com>
2021-02-11 19:21:42 +0100

    Create views.sql

---
+++ views.sql
@@ -0,0 +1,11 @@
+CREATE VIEW IlośćKomentarzyPost AS
+SELECT P.Id, P.Treść, P.Ilość_Polubień, A.IlośćKomentarzy
+FROM Posty P
+INNER JOIN (
+    SELECT K.Id_Postu, COUNT(*) IlośćKomentarzy 
+    FROM Komentarze K
+    GROUP BY K.Id_Postu
+) A ON A.Id_Postu = P.Id
+WHERE P.Id = @X
+
+-----
