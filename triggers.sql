a0a372f6e9cafb83dd46042423d7775e55be8a5d
Grzegorz Brzozowski <Dolidius@gmail.com>
2021-02-11 19:58:33 +0100

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -16,27 +16,30 @@ ON Posty
 INSTEAD OF DELETE 
 AS
 
-DECLARE @DeletedID INT
-SET @DeletedID = (
-    SELECT Id FROM Deleted
-)
-
-IF ((SELECT COUNT (*) FROM Posty P WHERE P.Id = @DeletedID) = 0) BEGIN
+IF ((SELECT COUNT (*) FROM Posty P INNER JOIN Deleted ON P.Id = Deleted.Id) = 0) BEGIN
     ROLLBACK
     RAISERROR('Nie istnieje post który próbowano usunąć', 16, 1)
 END
 
-IF ((SELECT COUNT (*) FROM Posty P WHERE P.Id = @DeletedID) > 1) BEGIN
+IF ((SELECT COUNT (*) FROM Posty P INNER JOIN Deleted ON P.Id = Deleted.Id) > 1) BEGIN
     ROLLBACK
     RAISERROR('Można usunąć tylko jeden post naraz', 16, 1)
 END
+
+DECLARE @DeletedID INT
+SET @DeletedID = (
+    SELECT Id FROM Deleted
+)
 
 DELETE FROM Posty
 WHERE Id = @DeletedID
 
 INSERT INTO Posty_Archiwum VALUES (Treść, Id_Autora, Id_Grupy, Ilość_Polubień, Data_Dodania, GETDATE(), 'usunięcie') 
 SELECT Treść, Id_Autora, Id_Grupy, Ilość_Polubień, Data_Dodania
-FROM Deleted 
+FROM Deleted
+
+DELETE FROM Komentarze
+WHERE Id_Postu = @DeletedID
 
 GO
 
