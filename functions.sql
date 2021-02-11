3184c692c75447d18900f521d7c1c810c8fc161c
Grzegorz Brzozowski <Dolidius@gmail.com>
2021-02-11 20:24:04 +0100

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,11 +1,11 @@
 --------------------
-CREATE OR ALTER FUNCTION PostyZGrupyXPoY (@X INT, @Y DATETIME)
+CREATE OR ALTER FUNCTION PostyZGrupyPoDacie (@Id_Grupy INT, @Data DATETIME)
 RETURNS TABLE
 AS
 RETURN (
     SELECT P.Id, P.Treść, P.Ilość_Polubień 
     FROM Posty P
-    WHERE P.Id_Grupy = @X AND P.Data_dodania >= @Y
+    WHERE P.Id_Grupy = @Id_Grupy AND P.Data_dodania >= @Data
 )
 GO
 ------------------
