b9fb28590168bf94e3eff5394c1f588bc64aed69
Grzegorz Brzozowski <Dolidius@gmail.com>
2021-02-12 15:47:05 +0100

    Update views.sql

--- views.sql
+++ views.sql
@@ -6,4 +6,15 @@ INNER JOIN (
     FROM Komentarze K
     GROUP BY K.Id_Postu
 ) A ON A.Id_Postu = P.Id
+GO
+
 -----
+
+CREATE VIEW RozpoczęteWydarzeniaSzczegóły AS
+SELECT W.Nazwa_Wydarzenia, W.Opis, Z.Ścieżka, A.Miasto, A.Ulica
+FROM Wydarzenia W
+INNER JOIN Zdjęcia Z ON Z.Id = W.Id_Zdjęcia
+INNER JOIN Adresy A ON A.Id = W.Id_Adresu 
+WHERE Godzina_Rozpoczęcia >= GETDATE() AND Godzina_Zakończenia <= GETDATE() 
+
+---------
